ae9bc8c5fe13bd371eb4b4f044c2f4b1f1131276
MBelangerB <47693085+MBelangerB@users.noreply.github.com>
2022-12-14 15:19:12 -0500

    Review Summoner model + prepare League table

--- migrations/20221210152722-InitialCreate_RiotAPITables.js
+++ migrations/20221210152722-InitialCreate_RiotAPITables.js
@@ -15,21 +15,26 @@ module.exports = {
         type: DataTypes.UUID,
         field: 'id',
         primaryKey: true,
-        allowNull: false,    
-        defaultValue: Sequelize.UUIDV4,
-      },
-      riotAccountId: {
-        type: DataTypes.STRING(56),
-        field: 'riotAccountId',
         allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
       },
       riotId: {
         type: DataTypes.STRING(63),
         field: 'riotId',
         allowNull: false,
       },
+      riotAccountId: {
+        type: DataTypes.STRING(56),
+        field: 'riotAccountId',
+        allowNull: false,
+      },
+      riotProfileIconId: {
+        type: DataTypes.INTEGER,
+        field: 'riotProfileIconId',
+        allowNull: true,
+      },
       riotPuuid: {
-        type: DataTypes.STRING(78),
+        type: DataTypes.UUID, // STRING(78),
         field: 'riotPuuid',
         allowNull: false,
       },
@@ -42,20 +47,23 @@ module.exports = {
         type: DataTypes.INTEGER,
         field: 'riotSummonerLevel',
         allowNull: false,
-        defaultValue: true,
       },
       region: {
         type: DataTypes.STRING,
         field: 'region',
-        allowNull: false,     
+        allowNull: false,
+      },
+      expiredAPIKey: {
+        type: DataTypes.BOOLEAN,
+        field: 'expiredAPIKey',
+        defaultValue: false,
+        allowNull: false,
       },
       updateAt: {
         type: DataTypes.DATE,
         field: 'updateAt',
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
       },
-      // LastUpdate   (mettre a jour le niveau et vérifier si le pseudo a été modifié)
     });
 
     await queryInterface.addIndex('RIOT_Summoner', {
@@ -69,7 +77,7 @@ module.exports = {
       fields: ['riotSummonerName', 'region'],
       name: 'IDX_riot_summoner_SummonerNameRegion',
     });
-    
+
     // *************************
     // SummonerInfo History
     // *************************
@@ -96,7 +104,7 @@ module.exports = {
         type: DataTypes.STRING,
         field: 'riotSummonerName',
         allowNull: false,
-      }, 
+      },
       updateAt: {
         type: DataTypes.DATE,
         field: 'updateAt',
@@ -117,18 +125,203 @@ module.exports = {
       name: 'IDX_riot_SummonerHistory_summonerName',
     });
 
+    // *************************
+    // SummonerLeague
+    // *************************
+    await queryInterface.createTable('RIOT_SummonerLeague', {
+      id: {
+        type: DataTypes.UUID,
+        field: 'id',
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      // One to One
+      riotSummonerId: {
+        type: DataTypes.UUID,
+        field: 'riotSummonerId',
+        allowNull: false,
+        references: {
+          model: 'RIOT_Summoner',
+          key: 'id'
+        },
+      },
+      riotLeagueId: {
+        type: DataTypes.STRING,
+        field: 'riotLeagueId',
+        allowNull: false,
+      },
+      // Same riotId on table RIOT_Summoner
+      riotSummonerId: {
+        type: DataTypes.STRING,
+        field: 'riotSummonerId',
+        allowNull: false,
+      },
+      riotSummonerName: {
+        type: DataTypes.STRING,
+        field: 'riotSummonerName',
+        allowNull: false,
+      },
+      queueType: {
+        type: DataTypes.STRING,
+        field: 'queueType',
+        allowNull: false,
+      },
+      tier: {
+        type: DataTypes.STRING,
+        field: 'tier',
+        allowNull: false,
+      },
+      rank: {
+        type: DataTypes.STRING,
+        field: 'rank',
+        allowNull: false,
+      },
+      leaguePoints: {
+        type: DataTypes.INTEGER,
+        field: 'leaguePoints',
+        allowNull: false,
+      },
+      wins: {
+        type: DataTypes.INTEGER,
+        field: 'wins',
+        allowNull: false,
+      },
+      losses: {
+        type: DataTypes.INTEGER,
+        field: 'losses',
+        allowNull: false,
+      },
+      riotHotStreak: {
+        type: DataTypes.BOOLEAN,
+        field: 'riotHotStreak',
+        defaultValue: false,
+        allowNull: false,
+      },
+      riotVeteran: {
+        type: DataTypes.BOOLEAN,
+        field: 'riotVeteran',
+        defaultValue: false,
+        allowNull: false,
+      },
+      riotFreshBlood: {
+        type: DataTypes.BOOLEAN,
+        field: 'riotFreshBlood',
+        defaultValue: false,
+        allowNull: false,
+      },
+      riotInactive: {
+        type: DataTypes.BOOLEAN,
+        field: 'riotInactive',
+        defaultValue: false,
+        allowNull: false,
+      },
+      // One To One
+      riotSummonerMiniSeriesId: {
+        type: DataTypes.UUID,
+        field: 'riotSummonerMiniSeriesId',
+        allowNull: false,
+        references: {
+          model: 'RIOT_SummonerMiniSeries',
+          key: 'id'
+        },
+      },
+      // Last update
+      updateAt: {
+        type: DataTypes.DATE,
+        field: 'updateAt',
+        allowNull: false
+      },
+    });
+
+    await queryInterface.addIndex('RIOT_SummonerLeague', {
+      fields: ['id'],
+      unique: true,
+      type: 'UNIQUE',
+      name: 'PK_riot_SummonerLeague_Id',
+    });
+
+    await queryInterface.addIndex('RIOT_SummonerLeague', {
+      fields: ['riotSummonerId'],
+      name: 'IDX_riot_SummonerLeague_riotSummonerId',
+    });
+    
+    await queryInterface.addIndex('RIOT_SummonerLeague', {
+      fields: ['riotSummonerName'],
+      name: 'IDX_riot_SummonerLeague_riotSummonerName',
+    });
+
+    // *************************
+    // SummonerLeague MiniSeries
+    // *************************
+    await queryInterface.createTable('RIOT_SummonerMiniSeries', {
+      id: {
+        type: DataTypes.UUID,
+        field: 'id',
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      wins: {
+        type: DataTypes.INTEGER,
+        field: 'wins',
+        allowNull: false,
+      },
+      losses: {
+        type: DataTypes.INTEGER,
+        field: 'losses',
+        allowNull: false,
+      },
+      target: {
+        type: DataTypes.INTEGER,
+        field: 'target',
+        allowNull: false,
+      },
+      progress: {
+        type: DataTypes.STRING(10),
+        field: 'progress',
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.addIndex('RIOT_SummonerMiniSeries', {
+      fields: ['id'],
+      unique: true,
+      type: 'UNIQUE',
+      name: 'PK_riot_SummonerMiniSeries_Id',
+    });
+
+    // *************************
+    // SummonerLeague History
+    // *************************
+
+    // *************************
+    // StaticInfo (??)
+    // *************************
+
+    // *************************
+    // SeasonInfo (??)
+    // *************************
+
     //
+
 
   },
 
   /* eslint-disable-next-line no-unused-vars */
   down: async (queryInterface, DataTypes) => {
+    await queryInterface.removeIndex('RIOT_SummonerMiniSeries', 'PK_riot_SummonerMiniSeries_Id');
+    await queryInterface.removeIndex('RIOT_SummonerLeague', 'IDX_riot_SummonerLeague_riotSummonerName');
+    await queryInterface.removeIndex('RIOT_SummonerLeague', 'IDX_riot_SummonerLeague_riotSummonerId');
+    await queryInterface.removeIndex('RIOT_SummonerLeague', 'PK_riot_SummonerLeague_Id');
+    await queryInterface.removeIndex('RIOT_SummonerHistory', 'PK_riot_SummonerHistory_Id');
+    await queryInterface.removeIndex('RIOT_SummonerHistory', 'IDX_riot_SummonerHistory_summonerName');
     await queryInterface.removeIndex('RIOT_Summoner', 'PK_riot_summoner_SummonerId');
     await queryInterface.removeIndex('RIOT_Summoner', 'IDX_riot_summoner_SummonerNameRegion');
 
-    await queryInterface.removeIndex('RIOT_SummonerHistory', 'PK_riot_SummonerHistory_Id');
-    await queryInterface.removeIndex('RIOT_SummonerHistory', 'IDX_riot_SummonerHistory_summonerName');
 
+    await queryInterface.dropTable('RIOT_SummonerMiniSeries');
+    await queryInterface.dropTable('RIOT_SummonerLeague');
     await queryInterface.dropTable('RIOT_SummonerHistory');
     await queryInterface.dropTable('RIOT_Summoner');
   },
--- models/Riot/RIOT_Summoner.model.js
+++ models/Riot/RIOT_Summoner.model.js
@@ -16,11 +16,16 @@ module.exports = (sequelize, DataTypes) => {
                 sourceKey: 'id', // Source Key In SOURCE
                 onDelete: 'CASCADE',
             });
+
+            RIOT_Summoner.hasOne(models.RIOT_SummonerLeague, {
+                foreignKey: 'riotSummonerId', // Set FK name on TARGET
+                sourceKey: 'id', // Source Key In SOURCE
+            });
         }
 
 
         /* eslint-disable-next-line no-dupe-class-members */
-        static async addSummoner(riotId, accountId, puuid, name, level, region) {
+        static async addSummoner(riotId, accountId, puuid, name, level, profileIconId, region) {
             return await this.create({
                 id: uuidv4(),
                 riotAccountId: accountId,
@@ -28,13 +33,15 @@ module.exports = (sequelize, DataTypes) => {
                 riotPuuid: puuid,
                 riotSummonerName: name,
                 riotSummonerLevel: level,
+                riotProfileIconId: profileIconId,
                 region: region
             });
         }
 
         /**
-         * Get Summone by Summoner name for a specific region
-         * @param {*} id 
+         * Find the summoner by Summoner name for a specific region
+         * @param {*} name 
+         * @param {*} region 
          * @returns 
          */
          static async findSummonerBySummonerName(name, region) {
@@ -59,11 +66,12 @@ module.exports = (sequelize, DataTypes) => {
             return await this.findOne({ where: { riotAccountId: accountId } });
         }
 
-        async updateSummonerInfo(summonerName, summonerLevel) {
+        async updateSummonerInfo(summonerName, summonerLevel, profileIconId) {
               // Create History
               this.set({
                 riotSummonerName: summonerName,
                 riotSummonerLevel: summonerLevel,
+                riotProfileIconId: profileIconId,
                 updateAt: sequelize.literal('CURRENT_TIMESTAMP'),
             });
           
@@ -88,16 +96,20 @@ module.exports = (sequelize, DataTypes) => {
             unique: true,
             defaultValue: sequelize.UUIDV4,
         },
-        riotAccountId: {
-            type: DataTypes.STRING(56),
-            allowNull: false,
-        },
         riotId: {
             type: DataTypes.STRING(63),
             allowNull: false,
         },
+        riotAccountId: {
+            type: DataTypes.STRING(56),
+            allowNull: false,
+        },
+        riotProfileIconId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
         riotPuuid: {
-            type: DataTypes.STRING(78),
+            type: DataTypes.UUID, // STRING(78),
             allowNull: false,
         },
         riotSummonerName: {
@@ -107,17 +119,21 @@ module.exports = (sequelize, DataTypes) => {
         riotSummonerLevel: {
             type: DataTypes.INTEGER,
             allowNull: false,
-            defaultValue: true,
         },
         region: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        expiredAPIKey: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false,
+         },
         updateAt: {
             type: DataTypes.DATE,
             allowNull: false,
             defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-          },
+        },
     }, {
         sequelize,
         modelName: 'RIOT_Summoner',
